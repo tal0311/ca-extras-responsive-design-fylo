6b1dbfd1d3d18d38d95fb5e187079da68303202c
tal0311 <tal.amit0311@gmail.com>
2022-04-10 12:58:07 +0300

    base

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/ca-extras-responsive-fylo/',
+  base: '/ca-extras-responsive-design-fylo/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
